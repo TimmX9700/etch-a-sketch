60f65ac585767ea0b21ed972c23085e7c5a0041b
Timothy <timmx.1997@gmail.com>
2017-07-24 23:40:56 -0700

    v1.0 - ugly

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
 $(document).ready(function() {
   $("body").append("<div class='container'> aaa </div>");
   $(".container").append("<table class='grid'></table>");
-  $(".grid").append("<tbody></tbody>");
+  $(".grid").css("height", "480px");
+  $(".grid").css("width", "480px");
 
-  for (var i = 0; i < 16; i++) {
-    $("tbody").append("<tr></tr>");
-  }
-  for (var i = 0; i < 16; i++) {
-      $("tr").append("<td></td>");
-  }
+  createGrid(16);
+  colorSquare();
 
-  $("td").on("mouseover", function() {
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256)
-    $(this).css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+  $(".container").append("<button class='clear'> Clear </button>");
+  $(".container").append("<button class='change'> Change Size </button>");
+
+  $(".clear").click(function() {
+    clear();
   });
 
+  $(".change").click(function() {
+    var size = parseInt(prompt("What box size do you want?", "16"));
+    createGrid(size);
+  });
 });
+
+function createGrid(size){
+  $(".grid").find("tbody").remove();
+  $(".grid").append("<tbody></tbody>");
+
+  for (var i = 0; i < size; i++) {
+    $("tbody").append("<tr></tr>");
+  }
+  for (var i = 0; i < size; i++) {
+    $("tr").append("<td></td>");
+  }
+
+  clear();
+  colorSquare();
+}
+
+function colorSquare() {
+  $("td").on("mouseover", function() {
+    if( $(this).css("background-color") === "rgb(255, 255, 255)"){
+      var r = Math.floor(Math.random() * 256);
+      var g = Math.floor(Math.random() * 256);
+      var b = Math.floor(Math.random() * 256)
+      $(this).css("opacity", "0.2")
+      $(this).css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+    }
+    else {
+      $(this).css("opacity", "+=0.2");
+    }
+  });
+}
+
+function clear() {
+  $("td").css("background-color", "rgb(255, 255, 255)");
+  $("td").css("opacity", "1")
+
+}
